f46ad480ff0ed99b62f8abb63754e60f24544abf
Sven Waser <34583000+Svenwas3f@users.noreply.github.com>
2019-07-12 17:40:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Energy Air game bot
-// @namespace    https://github.com/Svenwas3f/energy-Air-game-Bot-2019/blob/master/bot.js
-// @updateURL    https://github.com/Svenwas3f/energy-Air-game-Bot-2019/blob/master/bot.js
+// @namespace    https://raw.githubusercontent.com/Svenwas3f/energy-Air-game-Bot-2019/master/bot.js
+// @updateURL    https://raw.githubusercontent.com/Svenwas3f/energy-Air-game-Bot-2019/master/bot.js
 // @version      0.4
 // @description  Automate the Energy Air Game
 // @author       Svenwas3f
@@ -10,4 +10,4 @@
 // ==/UserScript==
 
 
-window.alert("Game not available\nNo bot created\nFatal error");
+window.alert("Game not available\nNo bot created\nFatal error\nTest");
