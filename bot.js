ec88ba2a84f1180fb18b953c847a47a3e956c613
Sven Waser <34583000+Svenwas3f@users.noreply.github.com>
2019-07-12 16:42:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Energy Air game bot
-// @namespace    http://tampermonkey.net/
-// @version      1
+// @namespace    https://github.com/Svenwas3f/energy-Air-game-Bot-2019
+// @version      0.1
 // @description  try to take over the world!
 // @author       Svenwas3f
 // @match        https://game.energy.ch
