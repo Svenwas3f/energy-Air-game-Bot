5b6fee1548c625ab7623c25706aef67564ecfe50
Sven Waser <34583000+Svenwas3f@users.noreply.github.com>
2019-07-12 16:40:55 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Energy Air game bot
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       Svenwas3f
+// @match        https://game.energy.ch
+// @grant        none
+// ==/UserScript==
+
+window.alert("Hello World");
