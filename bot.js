a1d9fafe869601885227dab5728ef1d786f5a829
Sven Waser <34583000+Svenwas3f@users.noreply.github.com>
2019-08-07 17:54:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Energy Air game bot
 // @namespace    https://raw.githubusercontent.com/Svenwas3f/energy-Air-game-Bot-2019/master/bot.js
-// @version      2.3
+// @version      2.4
 // @description  Automate the Energy Air Game
 // @author       Svenwas3f, RayJW
 // @match        https://game.energy.ch
