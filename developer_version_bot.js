7780601d3693cf71540825d56fb41a4e3673831a
Sven Waser <34583000+Svenwas3f@users.noreply.github.com>
2019-08-08 10:29:13 +0200

    Update developer_version_bot.js

--- developer_version_bot.js
+++ developer_version_bot.js
@@ -24,11 +24,11 @@ setInterval(
 
 setInterval(
     function() {
-        if(typeof(document.getElementsByClassName("title-verification")[0]) === 'undefined' && document.getElementsByClassName("title-verification")[0] === null){
+        if(typeof(document.getElementsByClassName("title-verification")[0]) == 'undefined' && document.getElementsByClassName("title-verification")[0] == null){
             location.reload();
         }
     },
-    10000
+    7500
 )
 
 setInterval(
@@ -76,7 +76,7 @@ function check_question(){
   //Check question
   var question = document.getElementsByClassName("question-text")[0].innerText;
 
-  for(var i = 0; i < q_and_a.length; i++){ 
+  for(var i = 0; i < q_and_a.length; i++){
     //Compare questions
     if(question.toLowerCase() == q_and_a[i][0].toLowerCase()){
       //Select answer and confirm
@@ -105,7 +105,7 @@ function page_load(){
   var phone_number = (typeof(document.getElementsByClassName("title-verification")[0]) != 'undefined' && document.getElementsByClassName("title-verification")[0] != null)?true:false;
   var ticket = (typeof(document.getElementsByClassName("circle col-xs-4 col-sm-3 col-md-4 col-lg-3")[0]) != 'undefined' && document.getElementsByClassName("circle col-xs-4 col-sm-3 col-md-4 col-lg-3")[0] != null)?true:false;
 
-  if(!phone_number){ 
+  if(!phone_number){
     if(question){
       check_question();
     }else if (ticket){
