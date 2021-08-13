34ee1c4e25d7615c0dd4012c87964ff0561ae37e
Svenwas3f <sven.waser@venusnet.ch>
2021-08-13 20:52:43 +0200

    Config

---
+++ developer.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Energy Air game bot - Developer version 2021
+// @namespace    https://raw.githubusercontent.com/Svenwas3f/energy-Air-game-Bot/master/developer.js
+// @version      0.0
+// @description  Automate the Energy Air Game
+// @author       Svenwas3f
+// @match        https://game.energy.ch
+// @grant        none
+// ==/UserScript==
